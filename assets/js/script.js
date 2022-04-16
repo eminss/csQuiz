948e44a73d507bf0a86a55030c63d4f49a10f013
Jaden Storey <jadenstorey@hotmail.com>
2022-04-16 14:31:06 -0700

    added correct or incorrect questions answer functionality, organized and refactored code

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,15 +4,24 @@
 // displays list of possible answers to question
 // if answered incorrectly subtract 10 seconds from timer, go to new question
 // if answered correctly, go to new question
+
+
 var timerText = document.getElementById('countdown');
 var startBtn = document.getElementById('startBtn');
 var questionUl = document.getElementById('optionsList');
 var questionDiv = document.getElementById('questionDiv');
 let questionEl = document.getElementById('currentQuestion');
 var amountCorrect = 0;
-let liEl;
+let li1 = document.getElementById('option1');
+let li2 = document.getElementById('option2');
+let li3 = document.getElementById('option3');
+let li4 = document.getElementById('option4');
+
+let liEl = [li1, li2, li3, li4];
 var timeLeft = '';
+
 // declare questions array
+
 let questions = [
     {
         question: 'What was the highest concurrent average players in CS:GO in a month?',
@@ -20,7 +29,7 @@ let questions = [
         answer: '991,640 players'
     },
     {
-        question: 'What is the most expensive cosemtic item for CS:GO ever sold?',
+        question: 'What is the most expensive cosmetic item for CS:GO ever sold?',
         options: ['$13.5k', '$10k', '$1k', '$150k'],
         answer: '$150k'
     },
@@ -35,10 +44,8 @@ let questions = [
         answer: 'AK-47'
     }
 ];
-var question1Options = questions[0].options;
-console.log(question1Options);
-var question1Answer = questions[0].answer;
-console.log(question1Answer);
+
+// let questionAnswers = [questions[0].options[3], questions[1].options[3], questions[2].options[2], questions[3].options[1]]
 
 function startGame() {
     startBtn.style.display = 'none';
@@ -67,84 +74,98 @@ function startGame() {
 
     // for loop to go through each li element and append it to the document
     for (let i = 0; i < questions.length; i++) {
-        liEl = document.createElement("li");
-        liEl.textContent = (i + 1) + ". " + questions[0].options[i];
-        questionUl.appendChild(liEl);
-        // console.log(liEl);
+        // liEl[i].textContent = (i + 1) + ". " + questions[0].options[i];
+        liEl[i].textContent = questions[0].options[i];
+        questionUl.appendChild(liEl[i]);
     }
-    questionUl.addEventListener("click", nextQuestion);
-    console.log(questionUl);
+    // click handlers to check if answer is correct or incorrect
+    for (let i = 0; i < 4; i++) {
+        liEl[i].addEventListener("click", function() {
+            if(liEl[i].textContent == questions[0].answer) {
+                amountCorrect++;
+                console.log(amountCorrect);
+                console.log(questions[i]);
+                // next question
+            } else if(liEl[i].textContent == questions[1].answer){
+                amountCorrect++;
+                console.log(amountCorrect);
+            } else if(liEl[i].textContent == questions[2].answer){
+                amountCorrect++;
+                console.log(amountCorrect);
+            } else if(liEl[i].textContent == questions[3].answer){
+                amountCorrect++;
+                console.log(amountCorrect);
+            }
+            else {
+                timeLeft = timeLeft - 10;
+            }
+            // console.log("clicked option " + i);
+            // console.log(questions[0].answer);
+            // console.log(liEl[i].textContent);
+            // console.log(i);
+            nextQuestion(questions);
+        })
+    }
 }
 
-function nextQuestion() {
-    if (questionUl == questions[0].answer) {
-        amountCorrect + 1;
-        timeLeft + 5;
-    } else {
-        timeLeft - 10;
-    }
-    questionEl.textContent = questions[1].question;
-    questionUl.textContent = "";
-    for (let i = 0; i < questions.length; i++) {
-        liEl = document.createElement("li");
-        liEl.textContent = (i + 1) + ". " + questions[1].options[i];
-        questionUl.appendChild(liEl);
-    }
-    questionUl.addEventListener("click", nextQuestion2);
-}
+// old WET click handler
+//     liEl[0].addEventListener("click", function(){
+//         timeLeft = timeLeft - 10;
+//         nextQuestion();
 
-function nextQuestion2() {
-    if (questionUl == questions[1].answer) {
-        amountCorrect + 1;
-        timeLeft + 5;
-    } else {
-        timeLeft - 10;
-    }
-    questionEl.textContent = questions[2].question;
-    questionUl.textContent = "";
-    for (let i = 0; i < questions.length; i++) {
-        liEl = document.createElement("li");
-        liEl.textContent = (i + 1) + ". " + questions[2].options[i];
-        questionUl.appendChild(liEl);
-    }
-    questionUl.addEventListener("click", nextQuestion3);
-}
+//     });
+//     liEl[1].addEventListener("click", function(){
+//         timeLeft = timeLeft - 10;
+//         nextQuestion();
 
-function nextQuestion3() {
-    if (questionUl == questions[3].answer) {
-        amountCorrect + 1;
-        timeLeft + 5;
-    } else {
-        timeLeft - 10;
-    }
-    questionEl.textContent = questions[3].question;
-    questionUl.textContent = "";
-    for (let i = 0; i < questions.length; i++) {
-        liEl = document.createElement("li");
-        liEl.textContent = (i + 1) + ". " + questions[3].options[i];
-        questionUl.appendChild(liEl);
-    }
-}
+//     });
+//     liEl[2].addEventListener("click", function(){
+//         timeLeft = timeLeft - 10;
+//         nextQuestion();
+
+//     });
+//     liEl[3].addEventListener("click", function(){
+//         amountCorrect++;
+//         nextQuestion();
+//         console.log(amountCorrect);
+//     });
+// }
+
+// function nextQuestion() {
+//     if (questionUl == questions[0].answer) {
+//         amountCorrect + 1;
+//         timeLeft + 5;
+//     } else {
+//         timeLeft - 10;
+//     }
+//     questionEl.textContent = questions[1].question;
+//     questionUl.textContent = "";
+//     for (let i = 0; i < questions.length; i++) {
+//         liEl = document.createElement("li");
+//         liEl.textContent = (i + 1) + ". " + questions[1].options[i];
+//         questionUl.appendChild(liEl);
+//     }
+//     questionUl.addEventListener("click", nextQuestion2);
+// }
 
 // function nextQuestion() {
 //     for (let i = 0; i < questions.length; i++) {
 //         questionEl.textContent = questions[i].question;
-//         // liEl = document.createElement("li");
-//         liEl.textContent = (i + 1) + ". " + questions[0].options[i];
-//         console.log(liEl);
+//         liEl[i].textContent = questions[i].options[i];
 //     }
-//     console.log("clicked option");
-//     liEl.addEventListener("click", nextQuestion);
+    // console.log("clicked option");
+    // liEl.addEventListener("click", nextQuestion);
 // }
 
 
-// function nextQuestion() {
-//     for (let i = 1; i < questions.length; i++) {
-//         for (let j = 0; j < questions.options[j].length; j++) {
-//             liEl.textContent = (j + 1) + ". " + questions[i].options[j];
-//         }
-//         questionEl.textContent = questions[i].question;
-//     }
-//     questionUl.addEventListener("click", nextQuestion);
-// }
+function nextQuestion(questions) {
+    for (let i = 1; i <= questions.length; i++) {
+        for (let j = 0; j < 4; j++) {
+            console.log(questions[i].options[j]);
+            liEl[j].textContent = questions[i].options[j];
+        }
+        questionEl.textContent = questions[i].question;
+    }
+    questionUl.addEventListener("click", nextQuestion);
+}
 startBtn.addEventListener("click", startGame);
